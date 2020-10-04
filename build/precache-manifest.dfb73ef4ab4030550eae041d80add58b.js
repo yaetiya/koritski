0d15cf1fa0e6c8e60b331c503f8f99a295676df3
yaetiya <yaetiya@gmail.com>
2020-10-05 00:40:28 +0300

    build

---
+++ build/precache-manifest.dfb73ef4ab4030550eae041d80add58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e058da6750ad6c14e711627c9021cd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c60505eeb82d52b0156c",
+    "url": "/static/css/main.6bf4803f.chunk.css"
+  },
+  {
+    "revision": "a75ae3459eaa137508e5",
+    "url": "/static/js/2.755fd6c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.755fd6c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60505eeb82d52b0156c",
+    "url": "/static/js/main.b409a21b.chunk.js"
+  },
+  {
+    "revision": "2dd1a96ca0c54b4e506f",
+    "url": "/static/js/runtime-main.961d0bc1.js"
+  }
+]);
